bb562d7c2c362c556f557c5e15ec317f7cc6ddbe
hongshuochen <hongshuo@usc.edu>
2023-05-09 00:17:35 -0700

    test

--- src/greenlab/example.py
+++ src/greenlab/example.py
@@ -1,2 +1,9 @@
+import torch
+
+
 def add_one(number):
     return number + 1
+
+
+def add_one_tensor(tensor):
+    return tensor + torch.ones_like(tensor)
---
+++ tests/test_example.py
@@ -0,0 +1,5 @@
+from greenlab.example import add_one
+
+
+def test_example_add_one():
+    assert add_one(1) == 2
